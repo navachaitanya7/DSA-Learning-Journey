1c4cf453e8dd9043f9bb83174e4fa93f0f70bde1
navachaitanya7 <navachaitanyayalamarthi@gmail.com>
2025-09-18 22:11:44 +0530

    Week-01: add addition-of-two-numbers (GFG)

---
+++ Week-01/addition-of-two-numbers.cpp
@@ -0,0 +1,22 @@
+// Addition of Two Numbers
+
+// //Given two numbers a and b. Your task is to return the sum of a and b.
+
+// Examples:
+
+// Input: a = 1, b = 2
+// Output: 3
+// Explanation: Addition of 1 and 2 is 3.
+// Input: a = 10, b = 20
+// Output: 30
+// Explanation: Addition os 10 and 20 is 30.
+// Constraints:
+// 1 <= a, b <= 1018
+ 
+class Solution {
+  public:
+    int addition(int a, int b) {
+      return a+b;
+        
+    }
+};
