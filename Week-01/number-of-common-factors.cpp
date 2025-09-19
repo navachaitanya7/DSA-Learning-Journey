ac8155b3b6e280360a65c73764d563cfe5ff52ad
navachaitanya7 <navachaitanyayalamarthi@gmail.com>
2025-09-19 22:21:29 +0530

    Week-01: add number-of-common-factors (LeetCode)

---
+++ Week-01/number-of-common-factors.cpp
@@ -0,0 +1,45 @@
+// 2427. Number of Common Factors
+
+// Given two positive integers a and b, return the number of common factors of a and b.
+
+// An integer x is a common factor of a and b if x divides both a and b.
+
+ 
+
+// Example 1:
+
+// Input: a = 12, b = 6
+// Output: 4
+// Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+// Example 2:
+
+// Input: a = 25, b = 30
+// Output: 2
+// Explanation: The common factors of 25 and 30 are 1, 5.
+ 
+
+// Constraints:
+
+// 1 <= a, b <= 1000
+ 
+
+#include <iostream>
+#include <algorithm> // for gcd
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int g = __gcd(a, b); // greatest common divisor
+        int count = 0;
+
+        for (int i = 1; i * i <= g; i++) {
+            if (g % i == 0) {
+                count++; // i is a divisor
+                if (i != g / i) count++; // g/i is also a divisor (avoid double count)
+            }
+        }
+        return count;
+    }
+};
